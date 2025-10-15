fa390683eef6c683d67b18d6482400a282360bfe
Brian Mikinski <brian.mikinski@gmail.com>
2025-10-14 23:15:38 -0500

    Refactor WasmLoader to use HelloModule interface for improved type safety

--- ng-wasm-app/src/app/wasm-loader.ts
+++ ng-wasm-app/src/app/wasm-loader.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 
+interface HelloModule {
+  ccall: (
+    ident: string,
+    returnType: string | null,
+    argTypes: string[],
+    args: unknown[]
+  ) => any;
+  UTF8ToString: (ptr: number) => string;
+}
+
 declare global {
   interface Window {
-    createHelloModule: any;
+    createHelloModule?: () => Promise<HelloModule>;
   }
 }
 
@@ -10,10 +20,10 @@ declare global {
   providedIn: 'root'
 })
 export class WasmLoader {
-  private module: any = null;
-  private moduleReady: Promise<any> | null = null;
+  private module: HelloModule | null = null;
+  private moduleReady: Promise<HelloModule> | null = null;
 
-  loadModule(): Promise<any> {
+  loadModule(): Promise<HelloModule> {
     if (this.moduleReady) {
       return this.moduleReady;
     }
@@ -23,7 +33,7 @@ export class WasmLoader {
       script.src = 'wasm/hello.js';
       script.onload = () => {
         if (window.createHelloModule) {
-          window.createHelloModule().then((module: any) => {
+          window.createHelloModule().then((module) => {
             this.module = module;
             resolve(module);
           }).catch(reject);
@@ -38,7 +48,7 @@ export class WasmLoader {
     return this.moduleReady;
   }
 
-  getModule(): any {
+  getModule(): HelloModule | null {
     return this.module;
   }
 }
